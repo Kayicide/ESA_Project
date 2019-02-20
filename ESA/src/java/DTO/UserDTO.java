2ce557bf63329f756bf245f0474d8e08d3207209
Charlie Ward <w015718h@student.staffs.ac.uk>
2019-02-20 14:20:04 +0000

    Created UserDTO
    Created and added the basic values for the USER DTO

---
+++ ESA/src/java/DTO/UserDTO.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+/**
+ *
+ * @author Kayde
+ */
+public class UserDTO {
+    private String UserID;
+    private String fullName;
+    private String address;
+    private int age;
+    private int passportNumber;
+    private boolean isAdmin;
+    
+    public UserDTO(String userID){
+        this.UserID = userID;
+    }
+    public UserDTO(String userID, String fullName, String address, int age, int passportNumber){
+        this(userID);
+        this.fullName = fullName;
+        this.address = address;
+        this.age = age;
+        this.passportNumber = passportNumber;
+    }
+    public UserDTO(String userID, String fullName, String address, int age, int passportNumber, boolean isAdmin){
+        this(userID, fullName,address, age, passportNumber);
+        this.isAdmin = isAdmin;
+    }
+
+    public String getUserID() {
+        return UserID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getPassportNumber() {
+        return passportNumber;
+    }
+
+    public boolean isIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setUserID(String UserID) {
+        this.UserID = UserID;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setPassportNumber(int passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    public void setIsAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+}
