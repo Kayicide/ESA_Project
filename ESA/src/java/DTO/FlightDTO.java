c6760d87f3672b34521a9f9b0303ba1c58f46384
Charlie Ward <w015718h@student.staffs.ac.uk>
2019-02-21 13:48:56 +0000

    Update FlightDTO.java

--- ESA/src/java/DTO/FlightDTO.java
+++ ESA/src/java/DTO/FlightDTO.java
@@ -17,8 +17,42 @@ public class FlightDTO {
     private Calendar departureDateTime;
     private String status;
     
-    public FlightDTO(int FlightID, RouteDTO route, Calendar depatureDateTime, String status){
-        
+    public FlightDTO(int flightID, RouteDTO route, Calendar departureDateTime, String status){
+        this.flightID = flightID;
+        this.route = route;
+        this.departureDateTime = departureDateTime;
+        this.status = status;
     }
-    
+
+    public int getFlightID() {
+        return flightID;
+    }
+
+    public RouteDTO getRoute() {
+        return route;
+    }
+
+    public Calendar getDepartureDateTime() {
+        return departureDateTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setFlightID(int flightID) {
+        this.flightID = flightID;
+    }
+
+    public void setRoute(RouteDTO route) {
+        this.route = route;
+    }
+
+    public void setDepartureDateTime(Calendar departureDateTime) {
+        this.departureDateTime = departureDateTime;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }   
 }
