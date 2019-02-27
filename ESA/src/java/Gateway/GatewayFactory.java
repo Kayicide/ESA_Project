eb990e7eb3e3522d44f4eece18b787908ba7a9f6
Scarlett <Scarlett@kayde.me>
2019-02-27 10:45:52 +0000

    Added some classes

---
+++ ESA/src/java/Gateway/GatewayFactory.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gateway;
+
+/**
+ *
+ * @author kayde
+ */
+public class GatewayFactory {
+    public GatewayAbstact create(int type){
+        switch(type){
+           default:
+               break;
+        }
+        return null;
+        
+    }
+}
