fcbb66c539e82cf50f493a32cbd1169384873455
tobiaslatham <lathamtobias@gmail.com>
2019-03-08 15:50:09 +0000

    completed some route gateway
    completed what i can of the route gateway

--- ESA/src/java/Gateway/PlaneGateway.java
+++ ESA/src/java/Gateway/PlaneGateway.java
@@ -7,29 +7,144 @@ package Gateway;
 
 import DTO.PlaneDTO;
 import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
  * @author Kayde
  */
 public class PlaneGateway extends GatewayAbstract{
+    
+    private Connection conn;
        
-    public boolean insert(PlaneDTO dto){
-        return true;  
+    public boolean insert(PlaneDTO plane){
+        boolean added = false;
+        
+        try
+        {
+            conn = database.getConnection();
+            
+            String sqlSt = "// SQL //";
+            PreparedStatement stmt = conn.prepareStatement(sqlSt);
+            stmt.setInt(1, plane.getPlaneID());
+            stmt.setInt(2, plane.getCapacity());
+            stmt.setInt(3, plane.getNoCrew());
+            stmt.setString(4, plane.getPlaneModel());
+            
+            int rows = stmt.executeUpdate();
+            added = rows == 1;
+            
+            stmt.close();
+            finishSQL(conn);
+        }
+        catch(SQLException ex)
+        {
+            ex.printStackTrace();
+            added = false;
+            finishSQL(conn);
+        }
+        
+        return added;  
     }
      
     public boolean delete(int id){
-       return true; 
+        boolean deleted = false;
+        
+        try
+        {
+            conn = database.getConnection();
+            
+            String sqlSt = "// SQL //";
+            PreparedStatement stmt = conn.prepareStatement(sqlSt);
+            stmt.setInt(1, id);
+            
+            int rows = stmt.executeUpdate();
+            deleted = rows == 1;
+            
+            stmt.close();
+            finishSQL(conn);
+        }
+        catch(SQLException ex)
+        {
+            ex.printStackTrace();
+            deleted = false;
+            finishSQL(conn);
+        }
+        
+       return deleted; 
     }
         
     public ArrayList<Object> getAll(){
         ArrayList<Object> PlaneList = new ArrayList<>();
+        
+        try
+        {
+            conn = database.getConnection();
+            
+            String sqlSt = "// SQL //";
+            PreparedStatement stmt = conn.prepareStatement(sqlSt);
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next())
+            {
+                PlaneDTO plane = new PlaneDTO(
+                        rs.getInt("planeID"),
+                        rs.getString("planeModel"),
+                        rs.getInt("capacity"),
+                        rs.getInt("noCrew"));
+                PlaneList.add(plane);
+            }
+            rs.close();
+            stmt.close();
+            finishSQL(conn);
+            
+        }
+        catch(SQLException ex)
+        {
+            ex.printStackTrace();
+            finishSQL(conn);
+        }
+        
         return PlaneList;
     }
     
     public Object getByID(int id){
-        PlaneDTO dto = new PlaneDTO(id, null, id, id);
-        return dto;
+        PlaneDTO plane = new PlaneDTO(id, null, id, id);
+        
+        try
+        {
+            conn = database.getConnection();
+            
+            String sqlSt = "// SQL //";
+            PreparedStatement stmt = conn.prepareStatement(sqlSt);
+            stmt.setInt(1, id);
+            
+            ResultSet rs = stmt.executeQuery();
+            
+            if(rs.next())
+            {
+                plane = new PlaneDTO(
+                        rs.getInt("planeId"),
+                        rs.getString("planeModel"),
+                        rs.getInt("capacity"),
+                        rs.getInt("noCrew"));
+            }
+            
+            rs.close();
+            stmt.close();
+            finishSQL(conn);
+            
+        }
+        catch(SQLException ex)
+        {
+            ex.printStackTrace();
+            finishSQL(conn);
+        }
+        
+        return plane;
     }
     
 }
--- ESA/src/java/Gateway/RouteGateway.java
+++ ESA/src/java/Gateway/RouteGateway.java
@@ -6,7 +6,12 @@
 package Gateway;
 
 import DTO.RouteDTO;
+import DTO.AirportDTO;
+import java.sql.Connection;
 import java.util.ArrayList;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -14,22 +19,134 @@ import java.util.ArrayList;
  */
 public class RouteGateway extends GatewayAbstract{
  
+    private Connection conn;
         
-    public boolean insert(RouteDTO dto){
+    public boolean insert(RouteDTO route){
+        boolean added = false;
+        
+        try
+        {
+            conn = database.getConnection();
+            String sqlSt = "// SQL //";
+            PreparedStatement stmt = conn.prepareStatement(sqlSt);
+            
+            AirportDTO destination = route.getDestination();
+            AirportDTO departureAirport = route.getDepartureAirport();
+            stmt.setInt(1, route.getRouteID());
+            stmt.setObject(2, destination);
+            stmt.setObject(3, departureAirport);
+            
+            int rows = stmt.executeUpdate();
+            added = rows == 1;
+            
+            stmt.close();
+            finishSQL(conn);
+        }
+        catch(SQLException ex)
+        {
+            ex.printStackTrace();
+            added = false;
+            finishSQL(conn);
+        }
+        
         return true;  
     }
      
     public boolean delete(int id){
+        boolean deleted = false;
+        
+        try
+        {
+            conn = database.getConnection();
+            String sqlSt = "// SQL //";
+            PreparedStatement stmt = conn.prepareStatement(sqlSt);
+            
+            stmt.setInt(1, id);
+            
+            int rows = stmt.executeUpdate();
+            deleted = rows == 1;
+            
+            stmt.close();
+            finishSQL(conn);
+        }
+        catch(SQLException ex)
+        {
+            ex.printStackTrace();
+            deleted = false;
+            finishSQL(conn);
+        }
+        
        return true; 
     }
        
     public ArrayList<Object> getAll(){
         ArrayList<Object> routeList = new ArrayList<>();
+        
+        try
+        {
+            conn = database.getConnection();
+            String sqlSt = "// SQL //";
+            PreparedStatement stmt = conn.prepareStatement(sqlSt);
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next())
+            {
+                AirportDTO destination = new AirportDTO(rs.getString("airportID"), null, rs.getString("destination"), 0, 0);
+                AirportDTO departureAirport = new AirportDTO(rs.getString("airportID"), null, rs.getString("destination"), 0, 0);
+                RouteDTO route = new RouteDTO(
+                        rs.getInt("routeID"),
+                        destination,
+                        departureAirport);
+                routeList.add(route);
+            }
+            
+            rs.close();
+            stmt.close();
+            finishSQL(conn);
+            
+        }
+        catch(SQLException ex)
+        {
+            ex.printStackTrace();
+            finishSQL(conn);
+        }
+        
         return routeList;
     }
     
     public Object getByID(int id){
-        RouteDTO dto = new RouteDTO(id, null, null);
-        return dto;
+        RouteDTO route = new RouteDTO(id, null, null);
+        
+        try
+        {
+            conn = database.getConnection();
+            String sqlSt = "// SQL //";
+            PreparedStatement stmt = conn.prepareStatement(sqlSt);
+            stmt.setInt(1, id);
+            
+            ResultSet rs = stmt.executeQuery();
+            
+            if(rs.next())
+            {
+                AirportDTO destination = new AirportDTO(rs.getString("airportID"), null, rs.getString("destination"), 0, 0);
+                AirportDTO departureAirport = new AirportDTO(rs.getString("airportID"), null, rs.getString("destination"), 0, 0);
+                route = new RouteDTO(
+                        rs.getInt("routeID"),
+                        destination,
+                        departureAirport);
+            }
+            
+            rs.close();
+            stmt.close();
+            finishSQL(conn);
+            
+        }
+        catch(SQLException ex)
+        {
+            ex.printStackTrace();
+            finishSQL(conn);
+        }
+        
+        return route;
     }
 }
