da0894eb6b19571445bd15361a1fdb48dee10d57
h024470h <35732843+h024470h@users.noreply.github.com>
2019-04-03 12:36:56 +0100

    Added BookingBean and FlightBean and started work on flight

---
+++ ESA/src/java/Bean/FlightBean.java
@@ -0,0 +1,31 @@
+
+package Bean;
+
+import DTO.FlightDTO;
+import Manager.FlightManager;
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.faces.bean.RequestScoped;
+import javax.inject.Named;
+
+@Named(value = "flightBean")
+@RequestScoped
+public class FlightBean implements Serializable {
+    
+    private FlightManager flightManager = new FlightManager();
+    
+    private FlightDTO flightdetails = null;
+    
+    public FlightDTO getFlightDetails(){
+        return flightdetails;
+    }
+        
+    public ArrayList<Object>getAllFlights(){
+        ArrayList<Object> allFlights = flightManager.getAll();
+        return allFlights;
+    }
+    
+    
+    
+    
+}
