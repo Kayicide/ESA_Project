19226100dfeb5ce961e7d411048ab228af4b2f5d
Charlie Ward <w015718h@student.staffs.ac.uk>
2019-05-03 15:20:43 +0100

    LoggedInFilter Done!

---
+++ ESA/src/java/Filter/LoggedInFilter.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Filter;
+
+import Bean.UserBean;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.faces.application.ResourceHandler;
+import javax.inject.Inject;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Kayde
+ */
+@WebFilter(filterName = "LoggedInFilter", urlPatterns = {"/faces/*"})
+public class LoggedInFilter implements Filter {
+    @Inject UserBean user;
+    private FilterConfig filterConfig = null;
+    public void doFilter(ServletRequest req, ServletResponse res,FilterChain chain)throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        String loginURI = request.getContextPath() + "/faces/login.xhtml";
+        String registerURI = request.getContextPath() + "/faces/register.xhtml";
+
+        boolean loggedIn = user.getLoggedIn();
+        boolean loginRequest = request.getRequestURI().equals(loginURI);
+        boolean registerRequest = request.getRequestURI().equals(registerURI);
+        boolean isStaticResource = request.getRequestURI().startsWith(request.getContextPath() + "/faces" + ResourceHandler.RESOURCE_IDENTIFIER);
+
+        if (loggedIn || loginRequest || registerRequest || isStaticResource)
+        {
+            chain.doFilter(request, response);
+        }
+        else
+        {
+            response.sendRedirect(loginURI);
+        }
+    }
+
+    public void destroy() {  
+        this.filterConfig = null;
+    }
+
+    public void init(FilterConfig filterConfig) {        
+        this.filterConfig = filterConfig;
+    }
+    
+    public void log(String msg){
+        filterConfig.getServletContext().log(msg);
+    }  
+    
+}
